d8ca0dd5f175891d7e5d7ce50527e7c2ea8b6b30
Timothy Jordan <ttjordan81@gmail.com>
2021-06-03 22:33:55 -0400

    add helper sidebar nav

--- resources/views/common/home-shelves.blade.php
+++ resources/views/common/home-shelves.blade.php
@@ -9,25 +9,7 @@
 @stop
 
 @section('right')
-    <div class="actions mb-xl c3">
-        <h5>{{ trans('common.actions') }}</h5>
-        <div class="home-right">
-            <p>New Hires</p>
-            <ul>
-                <li><a href="#">Hello</a></li>
-                <li><a href="#">ABC</a></li>
-            </ul>
-            <p>Upcoming</p>
-            <ul>
-                <li><a href="#">Hello</a></li>
-                <li><a href="#">ABC</a></li>
-            </ul>
-            <p>Things to do</p>
-            <ul>
-                <li>Hello</li>
-                <li>ABC</li>
-            </ul>
-        </div>
+        {!!  customSidbarNavigationForBlade() !!}
         <!--div class="icon-list text-primary">
             @include('partials.view-toggle', ['view' => $view, 'type' => 'shelves'])
             @include('components.expand-toggle', ['target' => '.entity-list.compact .entity-item-snippet', 'key' => 'home-details'])
